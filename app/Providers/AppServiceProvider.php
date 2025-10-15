49e3aab5fc9690572ac859cc915041f54dba4337
Prox-C <amadeusgravoso@gmail.com>
2025-10-15 13:50:44 +0800

    test 09

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
-            $_SERVER['HTTPS'] = 'on';
         }
     }
 }
