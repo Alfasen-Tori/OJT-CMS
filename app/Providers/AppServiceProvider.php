c18b70f797e21f6be01ee34eeea48b48af710cd2
Amadeus Gravoso <amadeusgravoso@gmail.com>
2025-10-20 22:54:48 +0800

    reverted back to sendgrid

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,15 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Mail\MailManager;
 use Illuminate\Support\Facades\URL;
-
+use Illuminate\Support\ServiceProvider;
+use SendGrid\Mail\Mail;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
@@ -20,13 +19,38 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
+        // Force HTTPS in production
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        // Use SendGrid Web API for email sending
+        $this->app->resolving(MailManager::class, function ($mailManager) {
+            $mailManager->extend('sendgrid', function () {
+                return new class {
+                    public function send($message)
+                    {
+                        $email = new Mail();
+                        $email->setFrom(
+                            config('mail.from.address'),
+                            config('mail.from.name')
+                        );
+
+                        foreach ($message->getTo() as $address => $name) {
+                            $email->addTo($address, $name);
+                        }
+
+                        $email->setSubject($message->getSubject());
+                        $email->addContent("text/html", $message->getBody());
+
+                        $sendgrid = new \SendGrid(config('services.sendgrid.api_key'));
+                        $sendgrid->send($email);
+                    }
+                };
+            });
+        });
     }
 }
