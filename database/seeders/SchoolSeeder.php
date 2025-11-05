3aabb9660d614912d4b7f105e9c91404750f6db9
Prox-C <amadeusgravoso@gmail.com>
2025-11-05 16:23:58 +0800

    updated school seeder to match actual program offerings

--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -11,14 +11,14 @@ class SchoolSeeder extends Seeder
     {
         $now = now();
 
-        // Colleges (column names: name, short_name)
+        // Colleges (column names: name, short_name)s
         $colleges = [
-            ['name' => 'Engineering', 'short_name' => 'COE'],
-            ['name' => 'Arts and Sciences', 'short_name' => 'CAS'],
-            ['name' => 'Education', 'short_name' => 'CED'],
-            ['name' => 'Technology', 'short_name' => 'COT'],
-            ['name' => 'Administration and Entrepreneurship', 'short_name' => 'CAE'],
-            ['name' => 'Architecture and Allied Discipline', 'short_name' => 'CAAD'],
+            ['name' => 'College of Architecture and Allied Discipline', 'short_name' => 'CAAD'],
+            ['name' => 'College of Arts & Sciences', 'short_name' => 'CAS'],
+            ['name' => 'College of Business & Entrepreneurship', 'short_name' => 'COBE'],
+            ['name' => 'College of Education', 'short_name' => 'COED'],
+            ['name' => 'College of Engineering', 'short_name' => 'COE'],
+            ['name' => 'College of Technology', 'short_name' => 'COT'],
         ];
 
         // Insert colleges (id will be the 'id' column)
@@ -37,40 +37,68 @@ class SchoolSeeder extends Seeder
 
         // Departments / programs (columns: dept_name, short_name, college_id)
         $departments = [
-            // Engineering (you listed these)
-            ['dept_name' => 'Information Technology',   'short_name' => 'IT',  'college_id' => $ids['Engineering']],
-            ['dept_name' => 'Civil Engineering',        'short_name' => 'CE',  'college_id' => $ids['Engineering']],
-            ['dept_name' => 'Mechanical Engineering',   'short_name' => 'ME',  'college_id' => $ids['Engineering']],
-            ['dept_name' => 'Electrical Engineering',   'short_name' => 'EE',  'college_id' => $ids['Engineering']],
-            ['dept_name' => 'Chemical Engineering',     'short_name' => 'ChE', 'college_id' => $ids['Engineering']],
-            ['dept_name' => 'Geodetic Engineering',     'short_name' => 'GE',  'college_id' => $ids['Engineering']],
-            ['dept_name' => 'Industrial Engineering',   'short_name' => 'IE',  'college_id' => $ids['Engineering']],
-            ['dept_name' => 'Electronics Engineering',  'short_name' => 'ECE', 'college_id' => $ids['Engineering']],
+            // College of Architecture and Allied Discipline
+            ['dept_name' => 'Bachelor of Science in Architecture', 'short_name' => 'BSARCH', 'college_id' => $ids['College of Architecture and Allied Discipline']],
+            ['dept_name' => 'Bachelor of Science in Interior Design', 'short_name' => 'BSID', 'college_id' => $ids['College of Architecture and Allied Discipline']],
 
-            // Arts and Sciences (examples)
-            ['dept_name' => 'English',                  'short_name' => 'ENG', 'college_id' => $ids['Arts and Sciences']],
-            ['dept_name' => 'Mathematics',              'short_name' => 'MATH','college_id' => $ids['Arts and Sciences']],
-            ['dept_name' => 'Political Science',        'short_name' => 'PS',  'college_id' => $ids['Arts and Sciences']],
+            // College of Arts & Sciences
+            ['dept_name' => 'Bachelor of Arts in English Language', 'short_name' => 'BAEL', 'college_id' => $ids['College of Arts & Sciences']],
+            ['dept_name' => 'Bachelor of Science in Economics', 'short_name' => 'BSECON', 'college_id' => $ids['College of Arts & Sciences']],
+            ['dept_name' => 'Bachelor of Science in Environmental Science', 'short_name' => 'BSES', 'college_id' => $ids['College of Arts & Sciences']],
+            ['dept_name' => 'Bachelor of Science in Chemistry', 'short_name' => 'BSCHEM', 'college_id' => $ids['College of Arts & Sciences']],
+            ['dept_name' => 'Bachelor of Science in Mathematics', 'short_name' => 'BSMATH', 'college_id' => $ids['College of Arts & Sciences']],
+            ['dept_name' => 'Bachelor of Science in Statistics', 'short_name' => 'BSSTAT', 'college_id' => $ids['College of Arts & Sciences']],
+            ['dept_name' => 'Batsilyer ng Sining sa Filipino', 'short_name' => 'BSFIL', 'college_id' => $ids['College of Arts & Sciences']],
 
-            // Education
-            ['dept_name' => 'Elementary Education',     'short_name' => 'BEED','college_id' => $ids['Education']],
-            ['dept_name' => 'Secondary Education',      'short_name' => 'BSED','college_id' => $ids['Education']],
-            ['dept_name' => 'Physical Education',       'short_name' => 'BPE', 'college_id' => $ids['Education']],
+            // College of Business & Entrepreneurship
+            ['dept_name' => 'Bachelor of Science in Accountancy', 'short_name' => 'BSA', 'college_id' => $ids['College of Business & Entrepreneurship']],
+            ['dept_name' => 'Bachelor of Science in Business Administration major in Marketing', 'short_name' => 'BSBA-MKT', 'college_id' => $ids['College of Business & Entrepreneurship']],
+            ['dept_name' => 'Bachelor of Science in Entrepreneurship', 'short_name' => 'BSENTREP', 'college_id' => $ids['College of Business & Entrepreneurship']],
+            ['dept_name' => 'Bachelor of Science in Office Administration', 'short_name' => 'BSOA', 'college_id' => $ids['College of Business & Entrepreneurship']],
 
-            // Technology
-            ['dept_name' => 'Automotive Technology',    'short_name' => 'AT',  'college_id' => $ids['Technology']],
-            ['dept_name' => 'Electronics Technology',   'short_name' => 'ET',  'college_id' => $ids['Technology']],
-            ['dept_name' => 'Drafting Technology',      'short_name' => 'DT',  'college_id' => $ids['Technology']],
+            // College of Education
+            ['dept_name' => 'Bachelor of Culture & Arts Education', 'short_name' => 'BCAED', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Elementary Education', 'short_name' => 'BEED', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Secondary Education major in Mathematics', 'short_name' => 'BSED-MATH', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Secondary Education major in Science', 'short_name' => 'BSED-SCI', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Physical Education', 'short_name' => 'BPE', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Automotive Technology', 'short_name' => 'BTVTED-AT', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Civil and Construction Technology', 'short_name' => 'BTVTED-CCT', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Electronics Technology', 'short_name' => 'BTVTED-ET', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Electrical Technology', 'short_name' => 'BTVTED-ELT', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Food and Service Management', 'short_name' => 'BTVTED-FSM', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Garments, Fashion and Design', 'short_name' => 'BTVTED-GFD', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Heating, Ventilating, Air-Conditioning and Refrigeration Technology', 'short_name' => 'BTVTED-HVAC', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Mechanical Technology', 'short_name' => 'BTVTED-MT', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technical-Vocational Teacher Education major in Welding and Fabrication Technology', 'short_name' => 'BTVTED-WFT', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technology & Livelihood Education major in Home Economics', 'short_name' => 'BTLE-HE', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Bachelor of Technology & Livelihood Education major in Industrial Arts', 'short_name' => 'BTLE-IA', 'college_id' => $ids['College of Education']],
+            ['dept_name' => 'Diploma in Teaching Secondary', 'short_name' => 'DTS', 'college_id' => $ids['College of Education']],
 
-            // Administration and Entrepreneurship
-            ['dept_name' => 'Business Administration',  'short_name' => 'BBA', 'college_id' => $ids['Administration and Entrepreneurship']],
-            ['dept_name' => 'Entrepreneurship',         'short_name' => 'ENT', 'college_id' => $ids['Administration and Entrepreneurship']],
-            ['dept_name' => 'Hospitality Management',   'short_name' => 'HM',  'college_id' => $ids['Administration and Entrepreneurship']],
+            // College of Engineering
+            ['dept_name' => 'Bachelor of Science in Civil Engineering', 'short_name' => 'BSCE', 'college_id' => $ids['College of Engineering']],
+            ['dept_name' => 'Bachelor of Science in Chemical Engineering', 'short_name' => 'BSCHE', 'college_id' => $ids['College of Engineering']],
+            ['dept_name' => 'Bachelor of Science in Electronics Engineering', 'short_name' => 'BSECE', 'college_id' => $ids['College of Engineering']],
+            ['dept_name' => 'Bachelor of Science in Electrical Engineering', 'short_name' => 'BSEE', 'college_id' => $ids['College of Engineering']],
+            ['dept_name' => 'Bachelor of Science in Geodetic Engineering', 'short_name' => 'BSGE', 'college_id' => $ids['College of Engineering']],
+            ['dept_name' => 'Bachelor of Science in Industrial Engineering', 'short_name' => 'BSIE', 'college_id' => $ids['College of Engineering']],
+            ['dept_name' => 'Bachelor of Science in Information Technology', 'short_name' => 'BSIT', 'college_id' => $ids['College of Engineering']],
+            ['dept_name' => 'Bachelor of Science in Mechanical Engineering', 'short_name' => 'BSME', 'college_id' => $ids['College of Engineering']],
 
-            // Architecture and Allied Discipline
-            ['dept_name' => 'Architecture',             'short_name' => 'ARCH','college_id' => $ids['Architecture and Allied Discipline']],
-            ['dept_name' => 'Interior Design',          'short_name' => 'ID',  'college_id' => $ids['Architecture and Allied Discipline']],
-            ['dept_name' => 'Landscape Architecture',   'short_name' => 'LA',  'college_id' => $ids['Architecture and Allied Discipline']],
+            // College of Technology
+            ['dept_name' => 'Bachelor of Science in Hospitality Management', 'short_name' => 'BSHM', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Industrial Technology major in Civil and Construction', 'short_name' => 'BSIT-CCT', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Industrial Technology major in Clothing and Fashion Design', 'short_name' => 'BSIT-CFD', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Industrial Technology major in Culinary Arts', 'short_name' => 'BSIT-CA', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Industrial Technology major in Electricity', 'short_name' => 'BSIT-ELEC', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Industrial Technology major in Electronics', 'short_name' => 'BSIT-ECE', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Industrial Technology major in Graphics Arts and Printing', 'short_name' => 'BSIT-GAP', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Industrial Technology major in Refrigeration and Air-Conditioning', 'short_name' => 'BSIT-RAC', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Mechanical Technology major in Automotive', 'short_name' => 'BSMT-AUTO', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Mechanical Technology major in Machine Shop', 'short_name' => 'BSMT-MS', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Mechanical Technology major in Metallurgy', 'short_name' => 'BSMT-MET', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Mechanical Technology major in Welding and Fabrication', 'short_name' => 'BSMT-WF', 'college_id' => $ids['College of Technology']],
+            ['dept_name' => 'Bachelor of Science in Nutrition and Dietetics', 'short_name' => 'BSND', 'college_id' => $ids['College of Technology']],
         ];
 
         // Insert departments (updateOrInsert avoids duplicates on repeated runs)
